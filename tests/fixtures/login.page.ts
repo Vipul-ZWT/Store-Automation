ffc0b43432fb6346cebb6bfa5865ca3510ad689b
Vipul <vipul@zealousweb.com>
2025-06-12 11:14:00 +0530

    Added the create account test case

--- tests/fixtures/login.page.ts
+++ tests/fixtures/login.page.ts
@@ -5,12 +5,24 @@ class LoginPage {
     readonly email:Locator;
     readonly password:Locator;
     readonly loginButton: Locator;
+    readonly firstName: Locator;
+    readonly lastName: Locator;
+    readonly registrationEmail: Locator;
+    readonly registrationPassword: Locator;
+    readonly passwordConfirmation: Locator;
+    readonly createAccountButton: Locator;
 
     constructor(page:Page){
         this.page = page;
         this.email = page.locator("input[name='login[username]']");
         this.password = page.locator("input[name='login[password]']");
         this.loginButton = page.getByRole('button', {name: 'Sign In'});
+        this.firstName = page.locator("#firstname");
+        this.lastName = page.locator("#lastname");
+        this.registrationEmail = page.locator("#email_address");
+        this.registrationPassword = page.locator("#password");
+        this.passwordConfirmation = page.locator("#password-confirmation");
+        this.createAccountButton = page.getByRole('button', {name: 'Create an Account'});
     }
 
     async login(email:string, password:string){
@@ -31,6 +43,19 @@ class LoginPage {
         });
 
         expect(loginStatus).toBe("1")
+    }
+
+    async createAccount(){
+        await this.page.goto(`${process.env.BASE_URL}/customer/account/create/`);
+        await this.firstName.fill(process.env.AFFILIATE_FIRST_NAME!);
+        await this.lastName.fill(process.env.AFFILIATE_LAST_NAME!);
+        await this.registrationEmail.fill(process.env.AFFILIATE_EMAIL!);
+        await this.registrationPassword.fill(process.env.AFFILIATE_PASSWORD!);
+        await this.passwordConfirmation.fill(process.env.AFFILIATE_PASSWORD_CONFIRMATION!);
+        await this.createAccountButton.scrollIntoViewIfNeeded();
+        await this.createAccountButton.click();
+
+        expect(this.page.waitForURL(`${process.env.BASE_URL}/customer/account/`)).toBeTruthy();
     }
 }
 
